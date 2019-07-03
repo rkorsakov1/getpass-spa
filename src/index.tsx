98471e68ac773964c1870a6cfa2b9f5bc379347f
stellarbears <stellarbears@gmail.com>
2019-07-03 10:25:24 +0300

    added default serviceworker for PWA

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from 'app';
 import "localization/i18n";
 import 'css/index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+serviceWorker.register();
